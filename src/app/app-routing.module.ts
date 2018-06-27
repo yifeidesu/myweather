afba2e4ac642c14d911fe7491fbc86deb322da6f
yifeidesu <yifeidesu@gmail.com>
2018-06-27 01:40:09 -0400

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
 
 ];
